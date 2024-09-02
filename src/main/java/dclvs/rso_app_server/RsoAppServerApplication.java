e00868c9e69dde472eb3a65172222e70a3dd3d66
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-03 00:02:37 +0300

    create project by spring boot init

---
+++ src/main/java/dclvs/rso_app_server/RsoAppServerApplication.java
@@ -0,0 +1,13 @@
+package dclvs.rso_app_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsoAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsoAppServerApplication.class, args);
+    }
+
+}
