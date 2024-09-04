ce34a2a059cb1d37f63de6c64e64e8249cc1eb0c
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-05 00:27:04 +0300

    made entity "user" mapping to table users

--- src/main/java/dclvs/rso_app_server/enteties/User.java
+++ src/main/java/dclvs/rso_app_server/enteties/User.java
@@ -1,4 +1,45 @@
 package dclvs.rso_app_server.enteties;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Calendar;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long user_id;
+
+    private String email;
+
+    private String password;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "surname")
+    private String surName;
+
+    @Column(name = "patronymic")
+    private String patronymicName;
+
+    @Temporal(TemporalType.DATE)
+    private Calendar birthday;
+
+    private String faculty;
+
+    private String speciality;
+
+    private Integer course;
+
+    private String group;
+
 }
