1fcc6fbc415e0f46ae73f424fb4993beb0eed4e6
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-05 22:04:34 +0300

    create event entity class to manipulating with it

---
+++ src/main/java/dclvs/rso_app_server/enteties/Event.java
@@ -0,0 +1,28 @@
+package dclvs.rso_app_server.enteties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.Calendar;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class Event {
+
+    private Long eventId;
+
+    private String tagName;
+
+    private String description;
+
+    private Calendar scheduledDate;
+
+    private Calendar beginAt;
+
+    private Calendar endsAt;
+
+}
