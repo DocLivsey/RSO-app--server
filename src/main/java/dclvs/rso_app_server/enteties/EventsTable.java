3674eadf61462df172a5f7e1c3fcc039e610cee2
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-05 22:01:34 +0300

    complete entity "event" mapping to event`s table

--- src/main/java/dclvs/rso_app_server/enteties/EventsTable.java
+++ src/main/java/dclvs/rso_app_server/enteties/EventsTable.java
@@ -1,4 +1,38 @@
 package dclvs.rso_app_server.enteties;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Calendar;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "events")
 public class EventsTable {
+
+    @Id
+    @GeneratedValue
+    private Long event_id;
+
+    @Column(name = "tag_name")
+    private String tagName;
+
+    private String description;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "scheduled_date")
+    private Calendar scheduledDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "begin_at")
+    private Calendar beginAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "ends_at")
+    private Calendar endsAt;
+
+
+
 }
