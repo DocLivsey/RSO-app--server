1e5b07574477bf3c87d545d6ddea3435ff031fb9
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-07 00:34:41 +0300

    complete user`s password class

--- src/main/java/dclvs/rso_app_server/enteties/components/Password.java
+++ src/main/java/dclvs/rso_app_server/enteties/components/Password.java
@@ -1,4 +1,69 @@
 package dclvs.rso_app_server.enteties.components;
 
+import dclvs.rso_app_server.Constants;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.regex.Pattern;
+
+@Slf4j
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Accessors(chain = true)
 public class Password {
+
+    private final Pattern VALID_PASSWORD_FORM = Pattern.compile(
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@#$%^&+=]).{8,}$",
+            Pattern.CASE_INSENSITIVE
+    );
+
+    /* In server service-part of application password might save and manipulated in sha hash-code
+     * Password will be transformed into hash-code in API service-part of application
+     * and receives as hash in dto from app`s API service
+     */
+    private String password;
+
+    public void setPassword(String password) {
+        try {
+            validatePassword(password);
+            this.password = password;
+        } catch (RuntimeException exception) {
+            log.error(exception.getMessage(), exception);
+        }
+    }
+
+    public Password(String password) {
+
+    }
+
+    public boolean isPasswordCorrect(String password) {
+        return VALID_PASSWORD_FORM
+                .matcher(password)
+                .matches();
+    }
+
+    public void validatePassword(String password) throws RuntimeException {
+        if (!isPasswordCorrect(password)) {
+            throw new RuntimeException( // TODO: code my own password throwable exception
+                    Constants.ANSI_RED_BACKGROUND +
+                            "password form is not correct! Validation did not pass" +
+                            Constants.RESET
+            );
+        } else {
+            log.info(
+                    Constants.ANSI_GREEN_BACKGROUND +
+                            "password is correct! Validation passed successfully" +
+                            Constants.RESET
+            );
+        }
+    }
+
+    public boolean matches(Password password) {
+        return false;
+    }
+
 }
