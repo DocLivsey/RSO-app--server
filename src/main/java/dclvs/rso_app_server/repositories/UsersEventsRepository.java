0385609aa2cf5d87ea0ccaf20578734b0dd77942
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-06 22:35:33 +0300

    complete repository for bind table for users and events

--- src/main/java/dclvs/rso_app_server/repositories/UsersEventsRepository.java
+++ src/main/java/dclvs/rso_app_server/repositories/UsersEventsRepository.java
@@ -1,13 +1,18 @@
 package dclvs.rso_app_server.repositories;
 
+import dclvs.rso_app_server.enteties.EventsTable;
 import dclvs.rso_app_server.enteties.UsersEventsTable;
 import dclvs.rso_app_server.enteties.UsersTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsersEventsRepository extends JpaRepository<UsersEventsTable, Long> {
 
-
+    @Query("select u from UsersEventsTable u where u.eventsTable == :eventsTable")
+    Optional<List<UsersTable>> getUsersByEvents(@Param("eventsTable") EventsTable eventsTable);
 
 }
