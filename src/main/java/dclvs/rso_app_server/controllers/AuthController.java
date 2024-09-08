1ba9e68f520b33f9364b618a56f00b1c224505c9
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-08 16:47:54 +0300

    вроде как complete user business logic

--- src/main/java/dclvs/rso_app_server/controllers/AuthController.java
+++ src/main/java/dclvs/rso_app_server/controllers/AuthController.java
@@ -1,8 +1,13 @@
 package dclvs.rso_app_server.controllers;
 
+import dclvs.rso_app_server.enteties.User;
 import dclvs.rso_app_server.services.AuthService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -12,14 +17,19 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @PostMapping("/user/signup")
     public void signup() {
 
     }
 
-    public void login() {
-
+    /* TODO: replace User -> UserDto and in AuthService make mapping UserDto into User
+        and do query to database by UserRepository with User not UserDto */
+    @GetMapping("/user/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        return authService.authenticate(user);
     }
 
+    @GetMapping("/user/logout")
     public void logout() {
 
     }
