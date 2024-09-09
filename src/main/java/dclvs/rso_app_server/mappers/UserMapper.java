cf0ba9191d7548bf21b71e37dedd126729872b7a
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-09 10:15:37 +0300

    add simple mapping start working on not simple mapping

--- src/main/java/dclvs/rso_app_server/mappers/UserMapper.java
+++ src/main/java/dclvs/rso_app_server/mappers/UserMapper.java
@@ -3,12 +3,28 @@ package dclvs.rso_app_server.mappers;
 import dclvs.rso_app_server.enteties.User;
 import dclvs.rso_app_server.enteties.UsersTable;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper
 public interface UserMapper {
 
+    @Mappings({
+            @Mapping(target = "email", source = "email"),
+            @Mapping(target = "password", source = "password"),
+            @Mapping(target = "faculty", source = "faculty"),
+            @Mapping(target = "speciality", source = "speciality"),
+            @Mapping(target = "group", source = "group"),
+    })
     public User toEntity(UsersTable usersTable);
 
+    @Mappings({
+            @Mapping(target = "email", source = "email", expression = "java(user.getEmail().getEmail())"),
+            @Mapping(target = "password", source = "password", expression = "java(user.getPassword().getPassword())"),
+            @Mapping(target = "faculty", source = "faculty"),
+            @Mapping(target = "speciality", source = "speciality"),
+            @Mapping(target = "group", source = "group"),
+    })
     public UsersTable toUsersTable(User user);
 
 }
