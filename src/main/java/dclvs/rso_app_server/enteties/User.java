c3c4880d5d46824bc453674eadc5369e620922ed
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-09-10 17:33:26 +0300

    upgrade enums to enums with constructors and added field as a value for enum`s field add method for parsing from RUS title

--- src/main/java/dclvs/rso_app_server/enteties/User.java
+++ src/main/java/dclvs/rso_app_server/enteties/User.java
@@ -4,29 +4,90 @@ import dclvs.rso_app_server.enteties.components.Email;
 import dclvs.rso_app_server.enteties.components.Password;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+
 import java.util.Calendar;
+import java.util.EnumSet;
+import java.util.concurrent.atomic.AtomicReference;
 
 /* TODO: replace enums to db`s entity-classes
 *   to allow CRUD manipulation with variable entities
 */
+@Getter
 enum Faculty {
-    FCSnAM,
+    FCSnAM("ФКТиПМ");
+
+    private final String titleRUS;
+
+    Faculty(String titleRUS) {
+        this.titleRUS = titleRUS;
+    }
+
+    public static Faculty fromTitle(String title) {
+        AtomicReference<Faculty> returnableFaculty = new AtomicReference<>();
+        EnumSet.allOf(Faculty.class) // all faculties from enum Faculty
+                .forEach(faculty -> {
+                    if (faculty.getTitleRUS().strip().equalsIgnoreCase(
+                            title.replaceAll(" ", "").strip())) {
+                        returnableFaculty.set(faculty);
+                    }
+                });
+        return returnableFaculty.get();
+    }
 }
 
+@Getter
 enum Speciality {
-    AMnCS, // Applied Math an Information
-    MS, // Math Support
-    AI, // Applied Informatics
+    AMnCS("ПМИ"), // Applied Math an Information
+    MS("МО"), // Math Support
+    AI("ПИ"); // Applied Informatics
+
+    private final String titleRUS;
+
+    Speciality(String titleRUS) {
+        this.titleRUS = titleRUS;
+    }
+
+    public static Speciality fromTitle(String title) {
+        AtomicReference<Speciality> returnableSpeciality = new AtomicReference<>();
+        EnumSet.allOf(Speciality.class)
+                .forEach(speciality -> {
+                    if (speciality.getTitleRUS().strip().equalsIgnoreCase(
+                            title.replaceAll(" ", "").strip())) {
+                        returnableSpeciality.set(speciality);
+                    }
+                });
+        return returnableSpeciality.get();
+    }
 }
 
+@Getter
 enum Group {
-    ThreeAM, FourAM,
-    ThreeMM, FourMM,
-    ThreeIT, FourIT,
-    ELEVENTH, TWELFTH, THIRTEENTH,
-    TWENTY_FIRST, TWENTY_SECOND, TWENTY_THIRTEENTH,
+    ThreeAM("3ПМ"), FourAM("4ПМ"),
+    ThreeMM("3ММ"), FourMM("4ММ"),
+    ThreeIT("3ИТ"), FourIT("4ИТ"),
+    ELEVENTH("11"), TWELFTH("12"), THIRTEENTH("13"),
+    TWENTY_FIRST("21"), TWENTY_SECOND("22"), TWENTY_THIRTEENTH("23");
+
+    private final String titleRUS;
+
+    Group(String titleRUS) {
+        this.titleRUS = titleRUS;
+    }
+
+    public static Group fromTitle(String title) {
+        AtomicReference<Group> returnableGroup = new AtomicReference<>();
+        EnumSet.allOf(Group.class)
+                .forEach(group -> {
+                    if (group.getTitleRUS().strip().equalsIgnoreCase(
+                            title.replaceAll(" ", "").strip())) {
+                        returnableGroup.set(group);
+                    }
+                });
+        return returnableGroup.get();
+    }
 }
 
 @Data
