995da1122997f9def053aafe1bd3680bcf20eb2c
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-08 02:55:28 +0300

    chore: some new jpa methods in future for requests from clients

--- src/main/java/dclvs/rso_app_server/repositories/EventRepository.java
+++ src/main/java/dclvs/rso_app_server/repositories/EventRepository.java
@@ -2,8 +2,11 @@ package dclvs.rso_app_server.repositories;
 
 import dclvs.rso_app_server.enteties.EventsTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,5 +16,10 @@ public interface EventRepository extends JpaRepository<EventsTable, Long> {
 
     @Query("select e from EventsTable e")
     Optional<List<EventsTable>> findAllEvents();
+
+    @Query("select e from EventsTable e where e.beginAt = :date")
+    Optional<List<EventsTable>> findAllEventsByDate(@Param("date") Calendar date);
+
+    void deleteByEventId(Long eventId);
 
 }
