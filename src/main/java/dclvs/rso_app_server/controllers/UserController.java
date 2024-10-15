4bf69db7d354765222d2b9e177fe015353ae6b1b
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-15 23:35:41 +0300

    feat: init rest controllers

--- src/main/java/dclvs/rso_app_server/controllers/UserController.java
+++ src/main/java/dclvs/rso_app_server/controllers/UserController.java
@@ -1,4 +1,11 @@
 package dclvs.rso_app_server.controllers;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@AllArgsConstructor
+@RestController("/user")
 public class UserController {
 }
