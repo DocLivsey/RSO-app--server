fef026f2c6fd75274cef99986029be02b59eecac
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-15 22:29:51 +0300

    feat: complete event's mapper

--- src/main/java/dclvs/rso_app_server/mappers/EventMapper.java
+++ src/main/java/dclvs/rso_app_server/mappers/EventMapper.java
@@ -2,11 +2,29 @@ package dclvs.rso_app_server.mappers;
 
 import dclvs.rso_app_server.enteties.Event;
 import dclvs.rso_app_server.enteties.EventsTable;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 public interface EventMapper {
 
+    @Mappings({
+            @Mapping(source = "eventId", target = "eventId"),
+            @Mapping(source = "tagName", target = "tagName"),
+            @Mapping(source = "description", target = "description"),
+            @Mapping(source = "scheduledDate", target = "scheduledDate"),
+            @Mapping(source = "beginAt", target = "beginAt"),
+            @Mapping(source = "endsAt", target = "endsAt")
+    })
     Event toEntity(EventsTable dataSource);
 
+    @Mappings({
+            @Mapping(source = "eventId", target = "eventId"),
+            @Mapping(source = "tagName", target = "tagName"),
+            @Mapping(source = "description", target = "description"),
+            @Mapping(source = "scheduledDate", target = "scheduledDate"),
+            @Mapping(source = "beginAt", target = "beginAt"),
+            @Mapping(source = "endsAt", target = "endsAt")
+    })
     EventsTable toDataSource(Event entity);
 
 }
