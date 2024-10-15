15fea8769dfc4aa5921e3063d570054478c416c0
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-15 09:20:11 +0300

    feat: create new service for event's entities logic

---
+++ src/main/java/dclvs/rso_app_server/services/EventsService.java
@@ -0,0 +1,20 @@
+package dclvs.rso_app_server.services;
+
+import dclvs.rso_app_server.mappers.EventMapper;
+import dclvs.rso_app_server.repositories.EventRepository;
+import lombok.AllArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+@Accessors(chain = true)
+public class EventsService {
+
+    private final EventMapper eventMapper;
+
+    private final EventRepository eventRepository;
+
+}
