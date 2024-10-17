df32284ffacd675660509ae04bda269c6e6a4a67
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-18 00:46:23 +0300

    feat: make get mapping on all users from event endpoint

--- src/main/java/dclvs/rso_app_server/controllers/UserController.java
+++ src/main/java/dclvs/rso_app_server/controllers/UserController.java
@@ -1,11 +1,42 @@
 package dclvs.rso_app_server.controllers;
 
+import dclvs.rso_app_server.Constants;
+import dclvs.rso_app_server.enteties.User;
+import dclvs.rso_app_server.services.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.rmi.NoSuchObjectException;
+import java.util.List;
+import java.util.function.Supplier;
 
 @Slf4j
 @AllArgsConstructor
 @RestController("/user")
 public class UserController {
+
+    private UserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<?> allUsersOnEvent(@RequestParam Long eventId) {
+        try {
+            List<User> users = userService
+                    .allUsersOnEvent(eventId)
+                    .orElseThrow(
+                            (Supplier<Throwable>) () -> new NoSuchObjectException(
+                                    STR."\{Constants.ERROR}There are no users on event with id: \{eventId}\{Constants.RESET}"
+                            )
+                    );
+            return ResponseEntity.ok(users);
+        } catch (NoSuchObjectException exception) {
+            return ResponseEntity.notFound().build();
+        } catch (Throwable throwable) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
 }
