57cb14051a2e07630713065de71604dc1a0fbeed
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-17 23:54:26 +0300

    feat: create new method that sends users

--- src/main/java/dclvs/rso_app_server/services/UserService.java
+++ src/main/java/dclvs/rso_app_server/services/UserService.java
@@ -1,14 +1,24 @@
 package dclvs.rso_app_server.services;
 
+import dclvs.rso_app_server.Constants;
+import dclvs.rso_app_server.enteties.Event;
+import dclvs.rso_app_server.enteties.EventsTable;
 import dclvs.rso_app_server.enteties.User;
 import dclvs.rso_app_server.enteties.UsersTable;
 import dclvs.rso_app_server.mappers.UserMapper;
+import dclvs.rso_app_server.repositories.EventRepository;
 import dclvs.rso_app_server.repositories.UserRepository;
 import dclvs.rso_app_server.repositories.UsersEventsRepository;
 import lombok.AllArgsConstructor;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.rmi.NoSuchObjectException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 @Slf4j
 @Service
@@ -20,6 +30,8 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final EventRepository eventRepository;
+
     private final UsersEventsRepository usersEventsRepository;
 
     public UsersTable convertToUsersTable(User user) {
@@ -28,6 +40,21 @@ public class UserService {
 
     public User convertToUser(UsersTable usersTable) {
         return userMapper.toEntity(usersTable);
+    }
+
+    public Optional<List<User>> allUsersOnEvent(Long eventId) throws Throwable {
+        EventsTable eventsTable = eventRepository
+                .findById(eventId)
+                .orElseThrow((Supplier<Throwable>) () -> new NoSuchObjectException(
+                        STR."\{Constants.ERROR}No such event by id: \{eventId} in system\{Constants.RESET}"
+                ));
+        return Optional.of(usersEventsRepository
+                .findUsersByEvent(eventsTable)
+                .stream()
+                .flatMap(Collection::stream)
+                .map(userMapper::toEntity)
+                .toList()
+        );
     }
 
 }
