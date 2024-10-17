76cfbae9b434fe0ff62b4ed26c288535e56dd81a
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-17 23:19:21 +0300

    feat: add get mapping that gave all events at date

--- src/main/java/dclvs/rso_app_server/controllers/EventController.java
+++ src/main/java/dclvs/rso_app_server/controllers/EventController.java
@@ -1,11 +1,43 @@
 package dclvs.rso_app_server.controllers;
 
+import dclvs.rso_app_server.Constants;
+import dclvs.rso_app_server.enteties.Event;
+import dclvs.rso_app_server.services.EventService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.rmi.NoSuchObjectException;
+import java.util.Calendar;
+import java.util.List;
+import java.util.function.Supplier;
 
 @Slf4j
 @AllArgsConstructor
 @RestController("/event")
 public class EventController {
+
+    private final EventService eventService;
+
+    @GetMapping("/all")
+    public ResponseEntity<?> allEventsAtDate(@RequestParam Calendar date) {
+        try {
+            List<Event> events = eventService
+                    .findAllEventsByDate(date)
+                    .orElseThrow(
+                            (Supplier<Throwable>) () -> new NoSuchObjectException(
+                                    STR."\{Constants.ERROR}There are no events on this date: \{date}\{Constants.RESET}"
+                            )
+                    );
+            return ResponseEntity.ok(events);
+        } catch (NoSuchObjectException exception) {
+            return ResponseEntity.badRequest().body(exception);
+        } catch (Throwable exception) {
+            return ResponseEntity.internalServerError().body(exception);
+        }
+    }
+
 }
