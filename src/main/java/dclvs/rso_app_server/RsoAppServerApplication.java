b491a473acf14d560b29b7d6a32d14e8fb5f5d1f
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-10-19 12:57:58 +0300

    feat: enable server http messaging

--- src/main/java/dclvs/rso_app_server/RsoAppServerApplication.java
+++ src/main/java/dclvs/rso_app_server/RsoAppServerApplication.java
@@ -2,7 +2,9 @@ package dclvs.rso_app_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class RsoAppServerApplication {
 
